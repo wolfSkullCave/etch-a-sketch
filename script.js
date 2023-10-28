f20ac3b46c4e2d712f03e0d80f09af924d6a1a1e
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2023-10-28 19:13:47 +0200

    Revert "setup for dynamic grid"
    This reverts commit 7ab022a54f6cae2495e5c5ae2a26a4ba940873a0.

---
+++ script.js
@@ -0,0 +1,49 @@
+<<<<<<< HEAD
+=======
+// create a 16x16 grid
+const container = document.querySelector('#container')
+
+// crate 16 rows each with 16 div block in them
+rowLoop:
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div')
+    container.appendChild(row)
+
+    columnLoop:
+    for (let j = 1; j <= 16; j++) {
+        // crate div
+        const div = document.createElement('div')
+        div.setAttribute('class', 'item')
+        // div.textContent = j
+        // add div to container
+        row.appendChild(div)
+    }
+}
+
+
+// trigger event on click
+const pixels = document.querySelectorAll('.item')
+
+pixels.forEach((pixel) => {
+    pixel.onclick = () => pixel.style.cssText = "background: black;"
+
+    pixel.addEventListener('mouseout', function(){
+        this.style.cssText = "background: black"
+    })
+})
+
+
+// eraser button
+const eraser = document.querySelector('#eraser')
+eraser.addEventListener('click', function(){
+    pixels.forEach( (pixel) => {
+        pixel.style.cssText = "background: lightgrey"
+    })
+})
+
+function testFunction() {
+    console.log('test')
+}
+
+
+>>>>>>> parent of 7ab022a (setup for dynamic grid)
