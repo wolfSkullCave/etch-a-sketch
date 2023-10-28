ddc220277ec178aeb4ba48f639c4bfda43a4542a
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2023-10-28 15:49:09 +0200

    Re-built the project. Added a grid button to change the amount of cells/pixels in the grid.

--- script.js
+++ script.js
@@ -1,46 +1,78 @@
-// create a 16x16 grid
-const container = document.querySelector('#container')
+// grid function
+function createGrid(blocks) {
+    const grid = document.querySelector('.grid')
 
-// crate 16 rows each with 16 div block in them
-rowLoop:
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div')
-    container.appendChild(row)
+    for (let i = 0; i < blocks; i++) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row')
+        grid.appendChild(row)
 
-    columnLoop:
-    for (let j = 1; j <= 16; j++) {
-        // crate div
-        const div = document.createElement('div')
-        div.setAttribute('class', 'item')
-        // div.textContent = j
-        // add div to container
-        row.appendChild(div)
+        for (let j = 0; j < blocks; j++) {
+            const cell = document.createElement('div')
+            cell.setAttribute('class', 'cell')
+            // cell.textContent = 'cell' + j
+
+            row.appendChild(cell)
+        }
+
     }
 }
 
 
-// trigger event on click
-const pixels = document.querySelectorAll('.item')
-
-pixels.forEach((pixel) => {
-    pixel.onclick = () => pixel.style.cssText = "background: black;"
-
-    pixel.addEventListener('mouseout', function(){
-        this.style.cssText = "background: black"
+function removeGrid(){
+    const rows = document.querySelectorAll('.row')
+    rows.forEach((row) => {
+        row.remove()
     })
-})
-
-
-// eraser button
-const eraser = document.querySelector('#eraser')
-eraser.addEventListener('click', function(){
-    pixels.forEach( (pixel) => {
-        pixel.style.cssText = "background: lightgrey"
-    })
-})
-
-function testFunction() {
-    console.log('test')
 }
 
 
+function draw() {
+    const cell = document.querySelectorAll('.cell')
+    cell.forEach((cells) => {
+        cells.addEventListener('mouseover', () => {
+            cells.setAttribute("style", "background-color: black;")
+
+        })
+    })
+}
+
+function eraseCells() {
+    const cell = document.querySelectorAll('.cell')
+    cell.forEach((cells) => {
+        cells.setAttribute("style", "background-color: lightgrey;")
+    })
+}
+
+// genereate grid 16x16
+createGrid(16)
+
+
+// get grid number
+const max = 64
+
+const grid = document.querySelector('#gridBtn')
+grid.onclick = () => {
+    let gridCells = prompt('Enter a grid number (max 64):')
+
+    // genereate new grid max 64
+    if (gridCells > 64) {
+        removeGrid()
+        createGrid(64)
+        draw()
+
+    } else {
+        removeGrid()
+        createGrid(gridCells)
+        draw()
+
+    }
+
+}
+
+// cell click event
+draw()
+
+// eraser button
+const eraserBtn = document.querySelector('#eraserBtn')
+eraserBtn.onclick = eraseCells
